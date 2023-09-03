7e684a964c2fe8f34b6542cb67a13aa6cad44357
Rick <101254647+nauish@users.noreply.github.com>
2023-09-03 21:01:26 +0800

    Add solutions

--- 04 - Array Cardio Day 1/javascript.js
+++ 04 - Array Cardio Day 1/javascript.js
@@ -64,20 +64,46 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-inventors.filter(years == 1500);
+const fifteen = inventors.filter(
+  (inventor) => inventor.year >= 1500 && inventor.year < 1600
+);
 
-console.log(inventors);
+console.table(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
+const invetorsFullName = inventors.map(
+  (inventor) => `${inventor.first} ${inventor.last}`
+);
+
+console.table(invetorsFullName);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const inventorsAgeDesc = inventors.sort((left, right) => {
+  if (left.year >= right.year) return 1;
+  else return -1;
+});
+
+console.table(inventorsAgeDesc);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.table(totalYears);
 
 // 5. Sort the inventors by years lived
+const inventorAge = inventors.sort((left, right) => {
+  const leftAge = left.passed - left.year;
+  const rightAge = right.passed - right.year;
+  if (leftAge <= rightAge) return 1;
+  else return -1;
+});
+
+console.table(inventorAge);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
